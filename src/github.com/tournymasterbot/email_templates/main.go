537ecdbaafc00675d38a903c9106c68af032bd46
TournyMasterBot <TournyMasterBot@users.noreply.github.com>
2020-09-08 17:20:32 -0500

    - Adds the ability to read the *.tokens file format

--- src/github.com/tournymasterbot/email_templates/main.go
+++ src/github.com/tournymasterbot/email_templates/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 
 	iohelper "github.com/tournymasterbot/io_helpers"
 )
+
+// TokenData : Raw token configuration details
+type TokenData struct {
+	Tokens Tokens `json:"tokens"`
+}
+
+// Tokens : Token details, primarily: Token substitution name and substitution value
+type Tokens []struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -22,15 +34,32 @@ func main() {
 		case "help":
 			fmt.Println("Available Commands: 'templates', 'tokens'")
 		case "templates":
+			// Read the template file
 			testTemplate, err := iohelper.ReadAllText("./templates/test.template")
-			if err == nil {
-				fmt.Println(testTemplate)
-			} else {
+			// Output the template file, or error
+			if err != nil {
 				fmt.Println(err)
+				return
 			}
+			fmt.Println(testTemplate)
 		case "tokens":
-			fmt.Println("token placeholder")
+			rawTokenData, err := iohelper.ReadAllBytes("./templates/test.tokens")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var data = TokenData{}
+			if err := json.Unmarshal(rawTokenData, &data); err != nil {
+				fmt.Println(err)
+				return
+			}
+			for i := 0; i < len(data.Tokens); i++ {
+				fmt.Println("Key: ", data.Tokens[i].Key, ", Value: ", data.Tokens[i].Value)
+				fmt.Println("---")
+			}
 		}
+		// Debug
 		if strings.Compare("hi", text) == 0 {
 			fmt.Println("hello, Yourself")
 		}
